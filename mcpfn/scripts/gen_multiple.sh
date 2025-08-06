aea710493848353ad1b65293751dbb096600a79f
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2025-08-05 23:07:21 -0400

    updated run multiple script for data generation to not do everything in parallel

--- mcpfn/scripts/gen_multiple.sh
+++ mcpfn/scripts/gen_multiple.sh
@@ -1,14 +1,14 @@
 # Multiple missingness types in a list
 
 missingness_types=("mcar" "mar" "mnar")
-columns=("5" "10" "20" "50")
-rows=("5" "10" "20" "50")
+columns=("5" "10" "20")
+rows=("5" "10" "20")
 
 for missingness_type in "${missingness_types[@]}"; do
     for column in "${columns[@]}"; do
         for row in "${rows[@]}"; do
             echo "Generating data for Missingness: ${missingness_type} Columns: ${column} Rows: ${row}"
-            nohup sh generate_data.sh ${missingness_type} scm ${column} ${row} > ${missingness_type}_${column}_${row}.log 2>&1 &
+            bash generate_data.sh ${missingness_type} scm ${column} ${row} > ${missingness_type}_${column}_${row}.log 2>&1 &
         done
     done
 done
