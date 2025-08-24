bf88e0e4f43af6a58acf9f573fca1988a329169c
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2025-08-24 21:08:19 +0000

    added some imputations

--- mcpfn/benchmark/get_openml_errors.py
+++ mcpfn/benchmark/get_openml_errors.py
@@ -20,17 +20,6 @@ def compute_abs_errors(X_true: torch.Tensor, X_missing: torch.Tensor, X_imputed:
     imputed_vals = torch.tensor(X_imputed)[mask]
     return torch.abs(true_vals - imputed_vals).numpy()
 
-def add_rows(rows_list, dataset_name, pattern_name, imputer_name, true_vals, imputed_vals):
-    for true_val, imputed_val in zip(true_vals, imputed_vals):
-        rows_list.append([dataset_name, pattern_name, imputer_name, true_val, imputed_val])
-
-# --- Define missingness patterns ---
-patterns = {
-    "MCAR": MCARPattern(config={"p_missing": 0.5}),
-    "MAR": MARPattern(config={"p_missing": 0.5}),
-    "MNAR": MNARPattern(config={"p_missing": 0.5}),
-}
-
 # --- Load imputer classes ---
 mcpfn = ImputePFN(
     device='cuda',
@@ -41,8 +30,6 @@ mcpfn = ImputePFN(
 tabpfn = TabPFNImputer(device='cuda')
 
 # --- Store all results ---
-results = {}
-
 rows = []
 
 base_path = "datasets/openml"
@@ -53,52 +40,40 @@ datasets = os.listdir(base_path)
 # --- Run benchmark ---
 for name in datasets:
     # Get filenames
-    filenames = os.listdir(f"{base_path}/{name}")
-    for filename in filenames:
-        if filename.endswith(".npy"):
-            pattern_name = filename.split("_")[0]
-            p = filename.split("_")[1]
-            X_missing = np.load(f"{base_path}/{name}/{filename}")
-            X_normalized = np.load(f"{base_path}/{name}/{pattern_name}_{p}_true.npy")
+    configs = os.listdir(f"{base_path}/{name}")
+    for config in configs:
+        print(f"Running {name} | {config}")
+        pattern_name = config.split("_")[0]
+        p = config.split("_")[1]
+        X_missing = np.load(f"{base_path}/{name}/{config}/missing.npy")
+        X_normalized = np.load(f"{base_path}/{name}/{config}/true.npy")
     
-    print(filenames)
-    exit()
-    
-    mask = torch.isnan(X_missing)
+        mask = np.isnan(X_missing)
 
-    # MCPFN
-    X_mcpfn = mcpfn.impute(X_missing.numpy().copy())
-    imputer_errors["MCPFN"] = compute_abs_errors(X_normalized, X_missing, X_mcpfn)
-    add_rows(rows, name, pattern_name, "MCPFN", X_normalized[mask], X_mcpfn[mask])
-    
-    # TabPFN
-    # X_tabpfn = tabpfn.impute(X_missing.numpy().copy())
-    # imputer_errors["TabPFN"] = compute_abs_errors(X_normalized, X_missing, X_tabpfn)
-    # add_rows(rows, name, pattern_name, "TabPFN", X_normalized[mask], X_tabpfn[mask])
-
-    # SoftImpute
-    plugin = Imputers().get("softimpute")
-    out = plugin.fit_transform(X_missing.numpy().copy()).to_numpy()
-    imputer_errors["SoftImpute"] = compute_abs_errors(X_normalized, X_missing, out)
-    add_rows(rows, name, pattern_name, "SoftImpute", X_normalized[mask], out[mask])
-    
-    # Column Mean
-    plugin = Imputers().get("mean")
-    out = plugin.fit_transform(X_missing.numpy().copy()).to_numpy()
-    imputer_errors["Column Mean"] = compute_abs_errors(X_normalized, X_missing, out)
-    add_rows(rows, name, pattern_name, "Column Mean", X_normalized[mask], out[mask])
-    
-    # HyperImpute
-    plugin = Imputers().get("hyperimpute")
-    out = plugin.fit_transform(X_missing.numpy().copy()).to_numpy()
-    imputer_errors["HyperImpute"] = compute_abs_errors(X_normalized, X_missing, out)
-    add_rows(rows, name, pattern_name, "HyperImpute", X_normalized[mask], out[mask])
-    
-    # Store results in dictionary
-    results[(name, pattern_name)] = imputer_errors
-    print(f"✅ {name} | {pattern_name} done.")
-    
-    torch.cuda.empty_cache()
+        # MCPFN
+        X_mcpfn = mcpfn.impute(X_missing.copy())
+        np.save(f"{base_path}/{name}/{config}/mcpfn.npy", X_mcpfn)
+        
+        # # TabPFN
+        # X_tabpfn = tabpfn.impute(X_missing.copy())
+        # np.save(f"{base_path}/{name}/{config}/tabpfn.npy", X_tabpfn)
+        
+        # SoftImpute
+        plugin = Imputers().get("softimpute")
+        out = plugin.fit_transform(X_missing.copy()).to_numpy()
+        np.save(f"{base_path}/{name}/{config}/softimpute.npy", out)
+        
+        # Column Mean
+        plugin = Imputers().get("mean")
+        out = plugin.fit_transform(X_missing.copy()).to_numpy()
+        np.save(f"{base_path}/{name}/{config}/column_mean.npy", out)
+        
+        # HyperImpute
+        # plugin = Imputers().get("hyperimpute")
+        # out = plugin.fit_transform(X_missing.copy()).to_numpy()
+        # add_rows(rows, name, pattern_name, "HyperImpute", X_normalized[mask], out[mask])
+        
+        # torch.cuda.empty_cache()
         
 # Output results to a csv file
 columns = ["dataset_name", "pattern_name", "imputer_name", "true_value", "imputed_value"]
